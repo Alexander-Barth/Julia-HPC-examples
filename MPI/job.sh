a8beba8fa08a5db6f33dad032ef95d35ed1ace19
Alexander Barth <barth.alexander@gmail.com>
2025-11-20 19:17:06 +0200

    for LUMI

--- MPI/job.sh
+++ MPI/job.sh
@@ -5,7 +5,14 @@
 #SBATCH --mem-per-cpu=700M
 #SBATCH --output=slurm-%x-%j.out
 
-# modules for Lucia (need to be adapted for other clusters)
-module load EasyBuild/2024a OpenMPI/5.0.3-GCC-13.3.0
+if module avail EasyBuild/2024a 2>&1 | grep -q EasyBuild/2024a; then
+    # modules for Lucia
+    module load EasyBuild/2024a OpenMPI/5.0.3-GCC-13.3.0
+elif module avail Local-CSC 2>&1 | grep -q Local-CSC; then
+    # modules for LUMI
+    module load Local-CSC julia-mpi
+#elif
+    # Load modules for other clusters
+fi
 
 srun julia --project=. "$@"
